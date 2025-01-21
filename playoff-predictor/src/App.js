dc3d05604688cd72380507dc88f54eeee4657160
Ciaran Burr <ciaranburr2005@gmail.com>
2025-01-21 00:53:32 -0500

    initialized different pages with different routes (not connected or decorated)

--- playoff-predictor/src/App.js
+++ playoff-predictor/src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
+import {AppRoutes} from './Routes'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Please pick one of the games and we will predict who will win:
-        </p>
-        <button>NFC Championship</button>
-        <button>AFC Championship</button>
-      </header>
-    </div>
+    <AppRoutes />
   );
 }
 
---
+++ playoff-predictor/src/pages/AfcChampionship.js
@@ -0,0 +1,5 @@
+export const AfcChampionship = () => {
+    return(
+        <p>hello</p>
+    );
+}
